ddc8966e62fce27d24ec130936e4044d23133f29
Elvin <elvin.azim01@gmail.com>
2022-12-01 10:10:08 +0400

    If conditions

---
+++ Conditions/Lesson1.py
@@ -0,0 +1,75 @@
+#  If Conditions
+
+# <editor-fold desc="Conditions">
+
+# num1 = int(input("Enter first number: "))
+# num2 = int(input("Enter first number: "))
+
+# <editor-fold desc="IF keyword">
+
+# if num1 == num2:
+#     print(num1)
+# if num1 > 0:
+#     print(num1)
+
+# </editor-fold>
+
+# <editor-fold desc="Else keyword">
+
+# if num1 == num2:
+#     print("Yes")
+# else:
+#     print("No")
+
+# if num1 == num2:
+#     print(f"{num1} == {num2}")
+# if num1 > num2:
+#     print(f"{num1} > {num2}")
+# else:
+#     print("No")
+
+
+# </editor-fold>
+
+# <editor-fold desc="Elif keyword">
+
+# if num1 == num2:
+#     print(f"{num1} == {num2}")
+# else:
+#     if num1 > num2:
+#         print(f"{num1} > {num2}")
+#     else:
+#         if num1 < num2:
+#             print(f"{num1} < {num2}")
+
+# if num1 == num2:
+#     print(f"{num1} == {num2}")
+#     print("{} == {}".format(num1, num2))
+# elif num1 > num2:
+#     print(f"{num1} > {num2}")
+# elif num1 < num2:
+#     print(f"{num1} < {num2}")
+
+# </editor-fold>
+
+# </editor-fold>
+
+#
+# choice = int(input("1. Addition\n"
+#                    "2. Subtraction\n"
+#                    "3. Multiplication\n"
+#                    "4. Division\n"))
+#
+# first_num = int(input("Enter first number: "))
+# second_num = int(input("Enter second number: "))
+#
+# if choice == 1:
+#     print(first_num + second_num)
+# elif choice == 2:
+#     print(first_num - second_num)
+# elif choice == 3:
+#     print(first_num * second_num)
+# elif choice == 4:
+#     print(first_num / second_num)
+# else:
+#     print("invalid input")
---
+++ Lesson2/main.py
@@ -0,0 +1,34 @@
+# <editor-fold desc="Data type casting">
+
+# <editor-fold desc="Explicit data type casting">
+
+# Explicit - Явно
+
+# number = 5
+# print(bool(number))
+
+# print(int(3.25 + 1.2))
+# print(1 + 0.)
+
+
+# </editor-fold>
+
+# <editor-fold desc="Implicit data type conversion">
+
+# print((5 < 6) + (1 < 2))
+# print((5 < 6).__add__((1 < 2)))
+#
+# number = 5
+#
+# if number:
+#     print("Number is equal to 5")
+
+# </editor-fold>
+
+# </editor-fold>
+
+# num = int(float(input("Enter your number: ")))
+# print(num)
+
+
+
