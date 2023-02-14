0f3676d9825941088b8e27d26309dd69eaf41524
Elvin <elvin.azim01@gmail.com>
2023-02-14 12:11:15 +0400

    C++

---
+++ C++/Repeat/First/First.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+#include <time.h>
+
+using namespace std;
+
+
+int main()
+{
+	//ctrl + k + c comment
+	//ctrl + k + u uncomment
+
+	//ctrl + shift + / comment/uncomment
+
+	// ctrl + k + f beautify code
+
+	/*cout << sizeof(int) << endl;
+	cout << sizeof(long) << endl;
+
+	cout << sizeof(long long) << endl;
+	cout << sizeof(long long int) << endl;*/
+
+	// 97 
+	//cout << (char)97 << endl; // explcit data type casting
+
+	//cout << ('a' + 325) << endl; // implicit data type casting 
+
+	//cout << (char)1427;
+
+	//int num = 2;
+
+	//if (num % 2 != 0)
+	//	cout << "Odd";
+	//else
+	//	if (num % 2 == 0)
+	//		cout << "Even";
+
+
+
+
+	//srand(time(0));
+	//
+	//int randomNumber = ((rand() % 10 + 1) - rand()) % 10;
+	//cout << randomNumber;
+
+
+	
+
+
+	return 0;
+}
+
---
+++ C++/While/While/While.cpp
@@ -0,0 +1,66 @@
+﻿#include <iostream>
+
+using namespace std;
+
+
+int main()
+{
+	/*
+	while (i != 10)
+	{
+		cout << ++i << endl;
+	}
+	*/
+
+	/*
+	while (i != 10)
+	{
+		cout << i++ << endl;
+	}
+	*/
+
+	/*int i{}, j{};
+
+	while (i < 3)
+	{
+		while (j < 3)
+		{
+			cout << i << '\t' << j << endl;
+			j++;
+		}
+		cout << endl;
+		j = 0;
+		i++;
+	}*/
+
+	// Цилкы бывают с постусловием и предусловием !!!
+
+	// for - это цикл с параметрами 
+
+	//for (int i = 0; i < 10; i++)
+	//{
+	//	cout << i << endl;
+	//}
+
+	//int i = 0;
+	//for (; i < 10; i++)
+	//{
+	//	cout << i << endl;
+	//}
+
+
+	// Endless for 
+	//for (;;)
+	//{
+		//cout << "Hello";
+	//}
+
+
+	/*for (int i = 0; i < 10; i++)
+	{
+		cout << i;
+	}*/
+	 
+
+    return 0;
+}
