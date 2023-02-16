a1a04bdac1651249f69052469f4e0008d06e6f62
Elvin <elvin.azim01@gmail.com>
2023-02-16 10:47:05 +0400

    For and array

---
+++ C++/Arrays/Arrays/Arrays.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+	int arr[8]{1, 2, 3, 4, 5, 6, 7, 8}; // Array - массив [10] - length
+
+	// * dereference operator
+	//for (size_t i = 0; i < 8; i++)
+	//{
+	//	cout << arr + i << '\t' << *(arr + i) << endl;
+	//}
+
+	/*for (size_t i = 0; i < 8; i++)
+	{
+		cout << arr[i] << ' ';
+	}*/
+
+
+	return 0;
+}
---
+++ C++/For/For/For.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	// size_t - unsinged long long 
+
+
+	//for (size_t i = 0; i < 4; i++)
+	//{
+	//	cout << "Begin" << endl;
+	//	for (size_t j = 0; j < 4; j++)
+	//	{
+	//		cout << i << ' ' << j << ' ';
+	//	}
+	//	cout << "END" << endl;
+	//}
+
+
+
+	//int a = 1;
+	//{
+	//	int b = 2;
+	//	{
+	//		int c = 3;
+	//		{
+	//			int d = 4;
+	//			{
+	//				int e = 5;
+	//			}
+	//			cout << '1';
+	//		}
+	//		cout << '2';
+	//	}
+	//	cout << '3';
+	//}
+	//cout << '4';
+
+
+
+	return 0;
+}
