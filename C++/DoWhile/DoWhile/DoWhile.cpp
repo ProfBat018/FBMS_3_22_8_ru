a90c27564ac8ee2c81e644ff6f19b2ed5d980284
Elvin <elvin.azim01@gmail.com>
2023-02-19 09:46:18 +0400

    Gitignore added

---
+++ C++/DoWhile/DoWhile/DoWhile.cpp
@@ -0,0 +1,35 @@
+﻿#include <iostream>
+using namespace std;
+
+
+int main()
+{
+	int i = 0;
+
+// Делает, потом думает
+#pragma region Do
+	
+	do
+	{
+		cout << i++;
+	} while (i == 10);
+
+
+#pragma endregion
+
+//  Думает, потом делает
+#pragma region While
+
+	/*while (i < 10)
+	{
+		cout << i++;
+	}*/
+
+	//while (i == 10)
+	//{
+	//	cout << i++;
+	//}
+#pragma endregion
+
+	return 0;
+}
