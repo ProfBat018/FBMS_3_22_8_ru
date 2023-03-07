5080e399c7e1f5705b43953a3c7b5ca8d1e77829
Elvin <elvin.azim01@gmail.com>
2023-03-07 11:06:19 +0400

    Overloading and template functions

--- C++/Arrays/Arrays/Arrays.cpp
+++ C++/Arrays/Arrays/Arrays.cpp
@@ -1,28 +1,18 @@
 ﻿#include <iostream>
+
 using namespace std;
+
+void foo() {
+    cout << "hello";
+}
+
+int foo(int a) {
+    cout << "hello";
+    return 1;
+}
+
 
 int main()
 {
-	//int arr[8]{1, 2, 3, 4, 5, 6, 7, 8}; // Array - массив [10] - length
-
-	// * dereference operator
-	//for (size_t i = 0; i < 8; i++)
-	//{
-	//	cout << arr + i << '\t' << *(arr + i) << endl;
-	//}
-
-	/*for (size_t i = 0; i < 8; i++)
-	{
-		cout << arr[i] << ' ';
-	}*/
-
-
-	int arr[]{ 1, 'a', true, 1.1 };
-
-	for (int i = 0; i < 4; ++i) {
-		cout << arr[i] << ' ';
-	}
-
-
-	return 0;
+    return 0;
 }
