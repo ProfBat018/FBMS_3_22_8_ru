dbd851b36857cc2e32ca9a20173b392098447b80
Elvin Azimov <elvin.azim01@gmail.com>
2023-04-06 11:42:19 +0400

    Git lesson 2

---
+++ C++/LibTestVS/LibTestVS/LibTestVS.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include "stringLib.h"
+
+using namespace std;
+
+int main()
+{
+    cout << getLength(new char[] {"Hello World"}) << endl;
+
+    return 0;
+}
---
+++ C++/StringLibVS/StringLibVS/StringLibVS.cpp
@@ -0,0 +1,6 @@
+#include "pch.h"
+#include "framework.h"
+
+void fnStringLibVS()
+{
+}
