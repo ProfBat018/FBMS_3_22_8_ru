f3a6188593c6b56f78474812015094f39a0677c1
Elvin Azimov <elvin.azim01@gmail.com>
2023-04-20 11:14:25 +0400

    Structs

--- C++/Repeat/Repeat.cpp
+++ C++/Repeat/Repeat.cpp
@@ -1,11 +1,18 @@
-// Repeat.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
 #include <iostream>
 
-int main()
-{
-       #pragma region Part1
+using namespace std;
+
+
+struct Student {
+    char* name{};
+    char* surname{};
+    char* patronimic{};
+    bool exams[5]{};
+    int marks[10]{};
+};
+
+int main() {
+#pragma region Part1
 
     /*
      int arr[2][3]
@@ -32,16 +39,51 @@ int main()
 //    cout << *(*(arr + 1) + 2) << endl;
 
 #pragma endregion
-    std::cout << "Hello World!\n";
+
+    
+#pragma region Part2
+
+    // Цикл может быть условным, итерационным и рекурсивным
+
+    // Условный цикл
+    /*
+    // Циклы бывают с предусловием, с постусловием и с условием внутри
+
+    // Предусловие
+    //    int i = 0;
+    //    while (i < 10)
+    //    {
+    //        cout << i << endl;
+    //        i++;
+    //    }
+
+    // Постусловие
+    //    int i = 0;
+    //    do
+    //    {
+    //        cout << i << endl;
+    //        i++;
+    //    } while (i < 10);
+
+    // Условие внутри
+    //    for (int i = 0; i < 10; i++)
+    //    {
+    //        cout << i << endl;
+    //    }
+
+    */
+
+    // Итерационный цикл
+
+    //    string name = "Elvin";
+    //
+    //    for (char i : name)
+    //    {
+    //        cout << i << endl;
+    //    }
+
+#pragma endregion
+
+
+    char *name = new char[10]{};
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
