aa65ead6dd119adbc084b102d501bf1459821be9
Elvin Azimov <elvin.azim01@gmail.com>
2023-08-22 11:58:29 +0400

    C# first lesson

---
+++ Intro/Intro/Program.cs
@@ -0,0 +1,83 @@
+﻿using System;
+
+#region Output
+
+//Console.WriteLine("Hello");
+//Console.WriteLine("World");
+
+//Console.Write("Hello");
+//Console.Write("World");
+
+
+#endregion
+
+#region ref&out
+
+//int sumNumbers(out int num1, out int num2)
+//{
+//    num1 = 5; num2 = 2;
+
+//    return num1 + num2;
+//}
+
+//int sumNumbers2(ref int num1, ref int num2)
+//{
+//    return num1 + num2;
+//}
+
+//int firstNum;
+//int secondNum;
+
+//sumNumbers(out firstNum, out secondNum);
+
+//Console.WriteLine(firstNum);
+//Console.WriteLine(secondNum);
+
+
+//sumNumbers2(ref firstNum, ref secondNum);
+
+
+#endregion
+
+#region Input 
+
+/*
+Console.Write("Enter first number: ");
+int num1 = Int32.Parse(Console.ReadLine());
+
+Console.Write("Enter second number: ");
+int num2 = Int32.Parse(Console.ReadLine());
+
+int res = num1 + num2;
+
+Console.WriteLine(res);
+*/
+
+//int num1, num2;
+
+//Console.Write("Enter first number: ");
+//bool num1Result = Int32.TryParse(Console.ReadLine(), out num1);
+
+//Console.Write("Enter second number: ");
+//bool num2Result = Int32.TryParse(Console.ReadLine(), out num2);
+
+//if (num1Result && num2Result)
+//{
+//    Console.WriteLine(num1 + num2);
+//}
+
+#endregion
+
+#region Arrays
+
+//int[] arr = new int[4] { 1, 2, 3, 4 };
+
+#endregion
+
+#region MultiArrays
+
+//int[][] arr1 = new int[2][] { new int[2] {1, 2}, new int[3] { 3, 4, 5 } }; // jagged array
+
+//int[,] arr2 = new int[,] { { 1, 2}, { 3, 4 } }; // multi array
+
+#endregion
