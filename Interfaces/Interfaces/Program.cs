444a97f01de6fcb48bd3b0fc8b437a068ce5045b
Elvin Azimov <elvin.azim01@gmail.com>
2023-09-03 11:14:25 +0400

    Leson 6

---
+++ Interfaces/Interfaces/Program.cs
@@ -0,0 +1,61 @@
+﻿
+// IDevice device = new Phone();
+// device.TurnOn();
+
+// Computer device = new();
+// IDevice d = device;
+// d.TurnOff();
+
+void BuyDevice(IDevice d)
+{
+    
+}
+
+
+class Phone : IDevice
+{
+    public void Call()
+    {
+        
+    }
+    public void TurnOn()
+    {
+        Console.WriteLine("Phone is turning on");
+    }
+
+    public void TurnOff()
+    {
+        Console.WriteLine("Phone is turning off");
+    }
+}
+
+
+class Computer : IDevice
+{
+    public void Foo()
+    {
+        
+    }
+
+    public void TurnOn()
+    {
+        Console.WriteLine("Computer is turning on");
+    }
+
+    public void TurnOff()
+    {
+        Console.WriteLine("Computer is turning off");
+    }
+}
+
+interface IDevice
+{
+    public void TurnOn();
+    public void TurnOff();
+}
+
+
+
+
+
+
