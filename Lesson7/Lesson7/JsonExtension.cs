1fdd062b36ae57a9ff88be1136f284a486ad9a80
Elvin Azimov <elvin.azim01@gmail.com>
2023-09-07 11:48:49 +0400

    Lesson7

---
+++ Lesson7/Lesson7/JsonExtension.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Text.Json.Serialization.Metadata;
+
+namespace Lesson7;
+
+public static class JsonExtension
+{
+   public static void AddInternalPropertiesModifier(JsonTypeInfo jsonTypeInfo)
+    {
+        if (jsonTypeInfo.Kind != JsonTypeInfoKind.Object)
+            return;
+
+        foreach (FieldInfo field in jsonTypeInfo.Type.GetFields(BindingFlags.Instance | BindingFlags.NonPublic))
+        {
+            
+            JsonPropertyInfo jsonPropertyInfo = jsonTypeInfo.CreateJsonPropertyInfo(field.FieldType, field.Name);
+            jsonPropertyInfo.Get = field.GetValue;
+            jsonPropertyInfo.Set = field.SetValue;
+
+            jsonTypeInfo.Properties.Add(jsonPropertyInfo);
+        }
+        
+        foreach (PropertyInfo property in jsonTypeInfo.Type.GetProperties(BindingFlags.Instance | BindingFlags.NonPublic))
+        {
+            
+            JsonPropertyInfo jsonPropertyInfo = jsonTypeInfo.CreateJsonPropertyInfo(property.PropertyType, property.Name);
+            jsonPropertyInfo.Get = property.GetValue;
+            jsonPropertyInfo.Set = property.SetValue;
+
+            jsonTypeInfo.Properties.Add(jsonPropertyInfo);
+        }
+        
+        
+
+    }
+}
