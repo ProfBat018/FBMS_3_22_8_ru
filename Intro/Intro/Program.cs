9e2742994d426f4f6416aa3d20432b117ce10606
WAYNE <wayne@WAYNEs-MacBook-Pro.local>
2024-01-07 11:31:08 +0400

    Updated

---
+++ Intro/Intro/Program.cs
@@ -0,0 +1,33 @@
+﻿using System.Data.SqlClient;
+using Microsoft.Extensions.Configuration;
+
+// Это по сути тот самый паттерн Builder
+
+var builder = new ConfigurationBuilder();
+builder.AddJsonFile("appsettings.json");
+
+
+var config = builder.Build();
+
+using SqlConnection conn = new(config.GetConnectionString("Default"));
+
+conn.Open();
+
+var command = new SqlCommand("select * from People", conn);
+
+using var res = command.ExecuteReader();
+
+while (res.Read())
+{
+    // Console.WriteLine($"{res[0]}\t{res[1]}");
+    Console.WriteLine(res.GetInt32(0));
+}
+
+
+
+
+
+
+
+
+
