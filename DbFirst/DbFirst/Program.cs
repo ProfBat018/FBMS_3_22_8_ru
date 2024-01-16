07e6b1c8ac4088f0ca836266e837cdf59c748793
wayne <profbat018@gmail.com>
2024-01-16 11:54:53 +0400

    Updated

---
+++ DbFirst/DbFirst/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using DbFirst;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+
+
+DbContextOptions<AcademyContext> ConfigStartup(string connectionName)
+{
+    ConfigurationBuilder builder = new();
+    builder.AddJsonFile("appsettings.json");
+
+    var config = builder.Build();
+
+    string? connectionString = config.GetConnectionString(connectionName);
+
+    var optionsBuilder = new DbContextOptionsBuilder<AcademyContext>();
+
+    DbContextOptions<AcademyContext>? options = optionsBuilder
+        .UseSqlServer(connectionString).Options;
+
+    return options ?? throw new NullReferenceException();
+}
+
+using AcademyContext academyContext = new(ConfigStartup("Default"));
+
+var res = academyContext.People.Where(x => x.Id > 5);
+
+foreach (var item in res)
+{
+    Console.WriteLine(item.Name);   
+}
+
+
+
---
+++ DbFirst/DbFirst/Student.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DbFirst;
+
+public partial class Student
+{
+    public int Id { get; set; }
+
+    public int? PersonId { get; set; }
+
+    public int? Gpa { get; set; }
+
+    public virtual ICollection<GroupDatum> GroupData { get; set; } = new List<GroupDatum>();
+
+    public virtual Person? Person { get; set; }
+}
