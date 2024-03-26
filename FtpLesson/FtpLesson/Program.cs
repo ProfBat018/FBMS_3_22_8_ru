71d0ce79fe3d4e55984c7f51a90aa77a6f30f185
wayne <profbat018@gmail.com>
2024-03-26 11:25:25 +0400

    Updated

---
+++ FtpLesson/FtpLesson/Program.cs
@@ -0,0 +1,61 @@
+﻿#region ListDirectory
+// using System.Net;
+//
+// var request = (FtpWebRequest)WebRequest.Create("ftp://test.rebex.net");
+//
+// request.Credentials = new NetworkCredential("demo", "password");
+//
+// request.Method = WebRequestMethods.Ftp.ListDirectory;
+//
+// var response = request.GetResponse().GetResponseStream();
+//
+// using StreamReader sr = new(response);
+//
+// Console.WriteLine(sr.ReadToEnd());
+
+
+#endregion
+
+
+#region DownloadFile
+//
+// using System.Net;
+//
+// var request = (FtpWebRequest)WebRequest.Create("ftp://test.rebex.net/readme.txt");
+//
+// request.Credentials = new NetworkCredential("demo", "password");
+//
+// request.Method = WebRequestMethods.Ftp.DownloadFile;
+//
+// var response = request.GetResponse().GetResponseStream();
+//
+// using Stream streamToWriteTo = File.Open("readme.txt", FileMode.Create);
+//
+// response.CopyTo(streamToWriteTo);
+
+#endregion
+
+#region UploadFile
+
+using System.Net;
+
+var request = (FtpWebRequest)WebRequest.Create("ftp://test.rebex.net/readme2.md");
+
+request.Credentials = new NetworkCredential("demo", "password");
+
+request.Method = WebRequestMethods.Ftp.UploadFile;
+
+var response = request.GetResponse().GetResponseStream();
+
+using var fs = new FileStream("/Users/wayne/Documents/Work/FBMS_3_22_8_RU/FtpLesson/FtpLesson/readme.md", FileMode.Open);
+
+fs.CopyTo(response);
+
+//
+// byte[] buffer = new byte[fs.Length];
+// fs.Read(buffer, 0, buffer.Length);
+//
+// response.Write(buffer, 0, buffer.Length);
+
+#endregion
+
---
+++ MailProtocols/MailProtocols/Program.cs
@@ -0,0 +1,30 @@
+﻿using System.Net;
+using System.Net.Mail;
+
+// var recipients = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+var recipient = "azimov_e@itstep.org";
+var subject = "Buenos Dias";
+var messageString = "Elnur bes";
+
+MailAddress fromAddress = new("profbat018@gmail.com", "Prof. Bat");
+MailAddress toAddress = new(recipient);
+
+MailMessage mailMessage = new(fromAddress, toAddress);
+
+mailMessage.Subject = subject;
+
+mailMessage.IsBodyHtml = true;
+// Можно просто считать все из FileStream, далее взять строку с помощью StreamReader и вставить все в Body 
+mailMessage.Body = $"""<html><body><h1>{messageString}</h1><img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ0cyduXNkqqHrdNMkJ2EqmncQbXgDri5sH6A&usqp=CAU"></img></body></html>""";
+
+mailMessage.Attachments.Add(new("/Users/wayne/Documents/Work/FBMS_3_22_8_RU/MailProtocols/MailProtocols/img.png")); // MIME 
+
+SmtpClient smtpClient = new("smtp.gmail.com", 587);
+
+smtpClient.Credentials = new NetworkCredential("profbat018@gmail.com", "enasbuejovhcbsnj");
+smtpClient.EnableSsl = true;
+
+smtpClient.Send(mailMessage);
+
+Console.WriteLine("Mail sent successfully");
