8b745934157a1cc5f0e87fd4eeec95345770cba3
wayne <profbat018@gmail.com>
2025-01-19 09:17:16 +0400

    updated

--- Prometheus/WebApplication1/WebApplication1/Controllers/HomeController.cs
+++ Prometheus/WebApplication1/WebApplication1/Controllers/HomeController.cs
@@ -7,11 +7,12 @@ namespace WebApplication1.Controllers;
 [Route("api/[controller]")]
 public class HomeController : ControllerBase
 {
-    private static readonly Counter SuccessfulAuthCounter = Metrics.CreateCounter("test_count", "Test counter");
+    private static readonly Counter SuccessfulTestCounter = Metrics.CreateCounter("test_count", "Test counter");
 
     [HttpGet("test")]
     public IActionResult Test()
     {
+        SuccessfulTestCounter.Inc();
         return Ok("Test");
     }
 }
